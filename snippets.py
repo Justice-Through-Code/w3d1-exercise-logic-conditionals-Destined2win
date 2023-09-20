1f0b765d747dfe3d294f5233d7050de30ccba4dc
Dre <developerdrequanwalker@gmail.com>
2023-09-20 13:02:17 -0400

    Run Code

--- snippets.py
+++ snippets.py
@@ -8,10 +8,11 @@ def snippet_1():
     u = 5
     v = 2
 
-    if u * v = 10:
+    if u * v == 10:
         print(f"The product of u ({u}) and v ({v}) is 10")
     else:
         print(f"The product of u ({u}) and v ({v}) is not 10")
+        snippet_1()
 
 
 def snippet_2():
@@ -23,10 +24,10 @@ def snippet_2():
     if z < x:
         print("z is less than x")
 
-    elif z > x and z < y
+    elif z > x and z < y:
         print("z is between x and y")
 
-    else
+    else:
         print("z is greater than y")
 
 
@@ -36,7 +37,7 @@ def snippet_3():
 
     a = 1
     b = 1
-    c = (a > b)
+    c = (a == b)
 
     print(f"The value of c ({c}) is True since a ({a}) is greater than b ({b}).")
     assert(c == True)  # <-- DO NOT EDIT THIS LINE
@@ -49,6 +50,7 @@ def snippet_4():
 
     # TODO: Explain how d is set to False in a comment
     assert(d == False)  # <-- DO NOT EDIT THIS LINE
+    snippet_4()
 
 
 def snippet_5():
@@ -58,7 +60,7 @@ def snippet_5():
     m = "GOAT"
     n = "goat"
 
-    o = (m == n)
+    o = (m.lower() == n.lower())
 
-    print (f"The value of o ({o}) is False since Python is case-sensitive.")
+    print (f"The value of o ({o}) is True since Python is case-sensitive.")
     assert(o == True)  # <-- DO NOT EDIT THIS LINE
